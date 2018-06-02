4a937fc75b3cae2969740b3234e3ea52841e363e
Vishvang <vishvangp.8@gmail.com>
2018-06-02 14:05:50 -0400

    Takes User Input and string interpolation feature.

--- angular-funda/src/app/user.component.ts
+++ angular-funda/src/app/user.component.ts
@@ -3,9 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
   template: `
-    <p>Hello</p>
+    <p>Hello, I am {{ name }} !</p>
+    <p><input type="text" (input)="onClickInput($event)"/></p>
     <p>I am the user component</p>
     `
 })
 
-export class UserComponent {}
+export class UserComponent {
+  name = 'Vishvang';
+
+  onClickInput(event) {
+    this.name = event.target.value;
+  }
+}
